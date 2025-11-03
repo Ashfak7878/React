22f3f861fbeeaa199bb810315c3dc825220a2e12
Ashfak Nasar <ashfaknasar7878@gmail.com>
2025-11-03 13:05:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Basic from './basic'
+import Welcome from './embadding-expression/Welcome'
+
+function App() {
+
+  return (
+    <>
+    {/* embading expression */}
+    {/* <Basic/> */}
+    <Welcome/>
+    </>
+  )
+}
+
+export default App
---
+++ src/embadding-expression/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+  let name = 'ashfak';
+  return (
+    <div>
+      <h3>hi  {name}</h3>
+    </div>
+  )
+}
+
+export default Welcome
